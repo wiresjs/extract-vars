e4d6ddb742a3a7dec1f5412d7b823cbfc8068599
Ivan.Orlov <ivan@morrr.com>
2016-10-08 23:12:58 +0300

    Build

--- dist/dist.js
+++ dist/dist.js
@@ -1,4 +1,4 @@
-(function($__exports__, $isBackend) {var __local__ = {};var define = function(n, d, f) {__local__[n] = { d: d, f: f }};var __resolve__ = function(name) {var m = __local__[name];if (m === undefined) {if ($isBackend) {return require(name);} else {Exports.__npm__ = Exports.__npm__ || {};return Exports.__npm__[name];}}if (m.r) { return m.r; }m.r = {};var z = [__resolve__, m.r];for (var i = 2; i < m.d.length; i++) {z.push(__resolve__(m.d[i]));}m.f.apply(null, z);return m.r;};
+(function($__exports__, $isBackend) {var __local__ = {};var define = function(n, d, f) {__local__[n] = { d: d, f: f }};var __resolve__ = function(name) {var m = __local__[name];if (m === undefined) {if ($isBackend) {return require(name);} else {$__exports__.__npm__ = $__exports__.__npm__ || {};return $__exports__.__npm__[name];}}if (m.r) { return m.r; }m.r = {};var z = [__resolve__, m.r];for (var i = 2; i < m.d.length; i++) {z.push(__resolve__(m.d[i]));}m.f.apply(null, z);return m.r;};
 define("ParserState", ["require", "exports"], function (require, exports) {
     "use strict";
     class ParserState {
@@ -273,15 +273,18 @@ define("index", ["require", "exports", "Digger"], function (require, exports, Di
 });
 
 var __expose__ = function(n, m, w, c) {
+    var cs = c ? c.split(",") : [];
+    if (cs.length) { for (var ln in __local__) { for (var i = 0; i < cs.length; i++) { if (ln.indexOf(cs[i]) === 0) { __resolve__(ln) } } } }
     var e = __resolve__(n);
     var bc;
-    if (!$isBackend) { var npm = $__exports__.__npm__ = $__exports__.__npm__ || {}; if (m) { bc = npm[m] } }
-    var cs = c ? c.split(",") : [];
-    if (cs.length){ for (var ln in __local__) { for (var i = 0; i < cs.length; i++) { if (ln.indexOf(cs[i]) === 0) { __resolve__(ln) } } }}
+    if (!$isBackend) { var npm = $__exports__.__npm__ = $__exports__.__npm__ || {}; if (m) { bc = npm[m] = {} } }
     for (var k in e) {
         $isBackend || w ? $__exports__[k] = e[k] : null;
-        bc ? bc[e] = e[k] : null;
+        bc ? bc[k] = e[k] : null;
     }
+
 };
 __expose__("index", "extract-vars", true, "");
-})(typeof exports !== "undefined" ? exports : this, typeof exports !== "undefined");
+})(typeof module !== "undefined" && module.exports && typeof process === "object" ?
+    exports : typeof window !== "undefined" ? window : this,
+    typeof module !== "undefined" && module.exports && typeof process === "object");
